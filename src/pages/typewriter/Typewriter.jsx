414286487741772b6084497a5580bfb72e708e9e
Ashutosh Nagrale <129092713+ashux25@users.noreply.github.com>
2024-01-15 11:16:39 +0000

    working on gsap typewriter effect

--- src/pages/typewriter/Typewriter.jsx
+++ src/pages/typewriter/Typewriter.jsx
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./typewriter.css";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
 
 const Typewriter = () => {
+  const words = ["Boldness", "Clarity", "Originality", "Precision"];
+
+  useEffect(() => {
+    gsap.registerPlugin(TextPlugin);
+    // Main Timeline
+    let mainTimeline = gsap.timeline({
+      repeat: -1,
+    });
+
+    // For each word , create a new timeline,
+    // use the Text Plugin,
+    // then append that timeline to main one
+    words.forEach((word) => {
+      let textTimeline = gsap.timeline({
+        repeat: 1,
+        yoyo: true,
+        repeatDelay: 6,
+      });
+
+      textTimeline.to("#typewriter", {
+        text: word,
+        duration: 1,
+        onUpdate: () => {
+          cursorTimeline.restart();
+          cursorTimeline.pause();
+        },
+        onComplete: () => {
+          cursorTimeline.play();
+        },
+      });
+
+      mainTimeline.add(textTimeline);
+    });
+
+    // Blinking cursorTimeline
+    let cursorTimeline = gsap.timeline({
+      repeat: -1,
+      repeatDelay: 0.8,
+    });
+
+    cursorTimeline
+      .to("#cursor", {
+        opacity: 1,
+        duration: 0,
+      })
+      .to("#cursor", {
+        opacity: 0,
+        duration: 0,
+        delay: 0.8,
+      });
+  }, []);
+
   return (
     <div className="typeContainer">
       <p>Typewriter Effect</p>
       <h1>
         Shaping Visions into Reality with
-        <span className="typewriter">Boldness</span>
+        <span id="typewriter"></span>
+        <span id="cursor">|</span>
       </h1>
     </div>
   );
