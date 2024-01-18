96e9c10d06b830203697f16e94bafec21ac379d4
AshutoshNagrale <ashutoshnagrale77@gmail.com>
2024-01-19 00:10:00 +0530

    done: getAllObjects, getObject, working on upload

--- src/pages/s3/S3.jsx
+++ src/pages/s3/S3.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./s3.css";
-import { getImagesUrlfromS3, listFilesInBucket } from "./s3Client";
+import { getImagesUrlfromS3, listFilesInBucket, main } from "./s3Client";
 
 const S3 = () => {
   // Images Names
@@ -11,13 +11,33 @@ const S3 = () => {
   // 1696786755590bbgfh.png
 
   const imageName = "1696777733401wbh.png";
+
   //GET image url from s3 using presigned url
   const getImage = async () => {
     const imageUrl = await getImagesUrlfromS3(imageName);
     return imageUrl;
   };
 
-  listFilesInBucket()
+  //Get ALL Objects from Bucket
+  const getAllObjects = () => {
+    const res = listFilesInBucket();
+    return res;
+  };
+
+  main()
+
+  return (
+    <>
+      <div className="s3Container">
+        <div className="title">S3 Integration</div>
+        <div className="s3Wrapper">
+          <div className="uploadImage">Upload Image</div>
+          <div className="uploadImage">Get Image</div>
+          <div className="uploadImage">Get ALL Image</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default S3;
--- src/pages/s3/s3Client.js
+++ src/pages/s3/s3Client.js
@@ -1,8 +1,9 @@
+import https from "https";
 import {
   S3Client,
   GetObjectCommand,
   ListObjectsCommand,
-  ListObjectsV2Command,
+  PutObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
@@ -10,8 +11,6 @@ const bucketname = import.meta.env.VITE_BUCKET_NAME;
 const bucketregion = import.meta.env.VITE_BUCKET_REGION;
 const accesskeys = import.meta.env.VITE_ACCESS_KEYS;
 const secretaccesskeys = import.meta.env.VITE_SECRET_ACCESS_KEYS;
-
-console.log(bucketname,bucketregion)
 
 const s3 = new S3Client({
   credentials: {
@@ -22,10 +21,10 @@ const s3 = new S3Client({
 });
 
 //Working
-export const getImagesUrlfromS3 = async (image) => {
+export const getImagesUrlfromS3 = async (filename) => {
   const getObjectParams = {
     Bucket: bucketname,
-    Key: image,
+    Key: filename,
   };
 
   const command = new GetObjectCommand(getObjectParams);
@@ -33,35 +32,67 @@ export const getImagesUrlfromS3 = async (image) => {
   return url;
 };
 
-// NOT WORKING
+//WORKING
 export const listFilesInBucket = async () => {
-  const input = { // ListObjectsRequest
+  const params = {
+    // ListObjectsRequest
     Bucket: bucketname, // required
-    MaxKeys: "2",
+    MaxKeys: "10",
   };
-  const command = new ListObjectsCommand(input);
-  const response = await s3.send(command);
-  console.log(response)
+
+  const command = new ListObjectsCommand(params);
+  const { Contents } = await s3.send(command);
+  const contentsList = Contents.map((c) => ` • ${c.Key}`).join("\n");
+  return contentsList;
 };
 
 // NOT WORKING
-export const postImagestoS3 = async (image) => {
-  const storage = multer.memoryStorage();
-  const upload = multer({ storage: storage });
+export const createPresignedUrlWithClient = ({ key }) => {
+  const command = new PutObjectCommand({ Bucket: bucketname, Key: key });
+  return getSignedUrl(s3, command, { expiresIn: 3600 });
+};
 
-  app.post("/api/upload", upload.single("file"), async (req, res) => {
-    console.log("req.body", req.body);
-    console.log("req.file", req.file);
-    const data = {
-      Bucket: bucketname,
-      Key: req.body.name,
-      Body: req.file.buffer,
-      ContentType: req.file.mimetype,
-    };
-
-    const command = new PutObjectCommand(data);
-    await s3.send(command);
-
-    res.send({});
+function put(url, data) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(
+      url,
+      { method: "PUT", headers: { "Content-Length": new Blob([data]).size } },
+      (res) => {
+        let responseBody = "";
+        res.on("data", (chunk) => {
+          responseBody += chunk;
+        });
+        res.on("end", () => {
+          resolve(responseBody);
+        });
+      }
+    );
+    req.on("error", (err) => {
+      reject(err);
+    });
+    req.write(data);
+    req.end();
   });
+}
+export const main = async () => {
+  const KEY = "src/assets/g2.jpg";
+
+  // There are two ways to generate a presigned URL.
+  // 1. Use createPresignedUrl without the S3 client.
+  // 2. Use getSignedUrl in conjunction with the S3 client and GetObjectCommand.
+  try {
+    const clientUrl = await createPresignedUrlWithClient({
+      key: KEY,
+    });
+
+    // After you get the presigned URL, you can provide your own file
+    // data. Refer to put() above.
+
+    console.log("Calling PUT using presigned URL with client");
+    await put(clientUrl, "Hello World");
+
+    console.log("\nDone. Check your S3 console.");
+  } catch (err) {
+    console.error(err);
+  }
 };
